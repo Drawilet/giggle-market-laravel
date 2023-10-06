e0b160203a354d17b296493f024d02c18947fa9c
Drawilet <drawiletdev@gmail.com>
2023-10-06 10:29:24 -0500

    Logo changed

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,1 +1,4 @@
-<img src="/icon.svg" alt="Giggle market" class="w-20">
+<div class="flex justify-center items-center">
+    <img src="/icon.svg" alt="Giggle market" class="w-20">
+    <h2 class="text-gray-100">Giggle Market</h2>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -392,7 +392,7 @@
 
 <body class="antialiased">
     <div
-        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-800 sm:items-center py-4 sm:pt-0">
         @if (Route::has('login'))
             <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                 @auth
@@ -413,7 +413,7 @@
             <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                 <img src="/icon.svg" alt="Giggle market" class="max-w-xs">
             </div>
-            <h1 class="text-center text-white">Welcome to Giggle Market</h1>
+            <h1 class="text-center dark:text-white">Welcome to Giggle Market</h1>
         </div>
     </div>
 </body>
