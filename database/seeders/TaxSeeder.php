6069ea2ffbf774d924a4a5a1bda7171199019256
Drawilet <drawiletdev@gmail.com>
2023-10-20 15:05:41 -0500

    Feature: the data amount of Tax and category seeders has been increased

--- database/seeders/TaxSeeder.php
+++ database/seeders/TaxSeeder.php
@@ -15,9 +15,24 @@ class TaxSeeder extends Seeder
      */
     public function run()
     {
-        DB::table("taxes")->insert([
-            "name" => "IVA (16%)",
-            "percentage" => 16,
-        ]);
+        $taxes = [
+            ["name" => "IVA (16)", "percentage" => 16],
+            ["name" => "Capital Gains Tax", "percentage" => 15],
+            ["name" => "Property Tax", "percentage" => 2.5],
+            ["name" => "Personal Property Tax (Luxury Cars)", "percentage" => 5],
+            ["name" => "Inheritance Tax", "percentage" => 10],
+            ["name" => "Stamp Duty", "percentage" => 1],
+            ["name" => "Special Tax (Alcohol)", "percentage" => 15],
+            ["name" => "Customs Duties (Cars)", "percentage" => 12],
+            ["name" => "Municipal Tax (Commercial Properties)", "percentage" => 3],
+            ["name" => "Local Sales Tax", "percentage" => 7],
+            ["name" => "Import Duty", "percentage" => 10],
+            ["name" => "Luxury Tax", "percentage" => 4],
+            ["name" => "Entertainment Tax", "percentage" => 12],
+        ];
+
+        foreach ($taxes as $tax) {
+            DB::table("taxes")->insert($tax);
+        }
     }
 }
