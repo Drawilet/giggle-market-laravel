9532897b8001fbb09476aff4159a6e3ca0edf30c
Drawilet <drawiletdev@gmail.com>
2023-11-01 11:22:48 -0500

    fixed: change notifications bg color

--- resources/views/components/bottom-alert.blade.php
+++ resources/views/components/bottom-alert.blade.php
@@ -16,9 +16,9 @@
     timeout = setTimeout(() => { shown = false }, 2000);
 })" x-show.transition.out.opacity.duration.1500ms="shown"
     x-transition:leave.opacity.duration.1500ms style="display: none;"
-    class="p-3 rounded  fixed bottom-0 right-0 bg-slate-800 mb-2 mr-2 flex flex-col">
+    class="p-3 rounded  fixed bottom-0 right-0 bg-base-300 mb-2 mr-2 flex flex-col">
 
-    <span class="text-gray-200 mb-2 px-2"> {{ $slot->isEmpty() ? 'Saved.' : $slot }}</span>
+    <span class=" mb-2 px-2"> {{ $slot->isEmpty() ? 'Saved.' : $slot }}</span>
 
     <span class="text-sm w-full rounded border-b" style="border-color: {{ $color }}"></span>
 </div>
